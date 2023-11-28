94d2b424e01511ec187a22224ca777c95d3183a1
maloevain <malo.evain@u-psud.fr>
2023-11-28 13:14:51 +0100

    t

--- Data/Scrapping/scrapping.py
+++ Data/Scrapping/scrapping.py
@@ -10,7 +10,7 @@ import pandas as pd
 """*******************************************************************************************************************"""
 
 """On commence par créer une liste des meilleurs serveurs de tous les temps"""
-"""
+
 cService = webdriver.ChromeService(executable_path='/Users/maloevain/Desktop/ENSAE2/S1/python/Projet/chromedriver')
 driver = webdriver.Chrome(service = cService)
 
@@ -31,12 +31,12 @@ for serveur in players:
     meilleurs_serveurs_alltime.append(serveur_info)
 
 driver.quit()
-"""
+
 """*******************************************************************************************************************"""
 
 "On va ensuite récolter les informations des meilleurs serveurs sur chaque année depuis 1991"
 
-"""
+
 serveur_list_1991_2022=[]
 
 for year in range(2016,2023):
@@ -70,13 +70,5 @@ df=pd.DataFrame(serveur_list_1991_2022+meilleurs_serveurs_alltime,columns=["year
 
 print(df.head())
 print(df.tail())
-df.to_csv('serveur_list')
-"""
-df1=pd.read_csv('meilleurs_serveurs_alltime')
-df2=pd.read_csv('service_info_1991_2022')
-df3=pd.read_csv('service_info_2005_2023')
-df4=pd.read_csv('serveur_list_2016_2023')
 
-df=pd.concat([df1,df2,df3,df4],ignore_index=True)
-print(df.head())
-df.to_csv('serveur_liste_finale')
+df.to_csv('serveur_list')
