f86d7294d644d251628da09c8629af5fcf58bc23
henrifabre123 <151456130+henrifabre123@users.noreply.github.com>
2023-11-28 16:30:44 +0100

    bon algo

--- scrapping/scraphenri.py
+++ scrapping/scraphenri.py
@@ -33,20 +33,21 @@ def scrape_return_stats(year):
     # Utiliser l'XPath pour extraire le tableau
     table = tree.xpath('//div[@id="statsListingTableContent"]/table')[0]
 
-   # Utiliser un sélecteur XPath différent pour extraire les en-têtes de colonnes
-    headers = [th.text.strip() if th.text else '' for th in table.xpath('.//thead//th[contains(@id, "Link")]/div[@class="sorting-inner"]/div[@class="sorting-label"]')]
-    print(headers)
 
-    # Extraire les données du tableau
+
     data = []
     for row in table.xpath('.//tbody//tr'):
-        row_data = [td.text.strip() if td.text else '' for td in row.xpath('.//td')]
-        print("Row data:", row_data)
+        player_name_elements = row.xpath('.//td[2]/div/a[@class="stats-player-name"]/text()')
+        player_name = player_name_elements[0].strip() if player_name_elements else 'N/A'
+        
+        # Extraire les autres données de la ligne
+        row_data = [player_name] + [td.text.strip() if td.text else '' for td in row.xpath('.//td')[1:]]  # Ajouter le nom du joueur en première colonne
+        #print("Row data:", row_data)
         data.append(row_data)
 
     # Créer un DataFrame Pandas
-    df = pd.DataFrame(data, columns=headers)
-
+    df = pd.DataFrame(data, columns = ['Name',' ','Return Rating','% 1 Serve Return Points Won','% 2nd Serve Return Points Won','% Return Games Won','% Break Points Converted'])
+    df['Year'] = year
 
 
     return df
